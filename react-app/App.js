da0a92c5f64c93f998d6ba3106900a5b2e043d01
diellmustafa <diell200908@gmail.com>
2024-11-25 17:43:26 +0100

    lesson17

--- react-app/App.js
+++ react-app/App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStackNavigator from './StackNavigator';
+import BottomTabNavigator from './TabNavigation';
 
 export default function App() {
   return (
+    // <NavigationContainer>
+    //   <MainStackNavigator></MainStackNavigator>
+    // </NavigationContainer>
     <NavigationContainer>
-      <MainStackNavigator></MainStackNavigator>
+      <BottomTabNavigator></BottomTabNavigator>
     </NavigationContainer>
   );
 }
